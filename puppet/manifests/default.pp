d2a10838d8538630681bb42c2af454019e7feaf8
Dan Davison <dan@counsyl.com>
2015-05-09 17:06:02 -0700

    Clone fpnp git repo

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -5,3 +5,9 @@ package { 'git':
 package { 'docker.io':
   ensure => 'installed',
 }
+
+vcsrepo { '/home/vagrant/fopnp':
+  provider => 'git',
+  source => 'https://github.com/brandon-rhodes/fopnp.git',
+  ensure => 'latest',
+}
